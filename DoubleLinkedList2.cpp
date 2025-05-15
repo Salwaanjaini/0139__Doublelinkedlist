0fc72a11c0fcaa5fde2fe8c64873caa0da52bcfc
Salwaanjaini27 <salwaanjainifutriendsani@gmail.com>
2025-05-15 15:29:48 +0700

    memasukan step 5

--- DoubleLinkedList2.cpp
+++ DoubleLinkedList2.cpp
@@ -43,6 +43,9 @@ public:
             }
             // step 4 : newNode.next = START
             newNode->next = START;
+            // step 5 : Start.prev = newNode (if START exists)
+            if (START != NULL)
+                START->prev = newNode;
         }
     }
 };
